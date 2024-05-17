2005fa7425588e9a19747b0c0a63d1b8f8e18aeb
ArdataCode <ardata.indonesia@gmail.com>
2024-05-17 16:13:10 +0700

    mitt

---
+++ wp-salt.php
@@ -0,0 +1,17 @@
+<?php
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ */
+define('AUTH_KEY',         '7WS7Sbb3qbuAGnVD53Qo9DaFvR2hBhVi9Ci81NtKh4Qq9FxtuUbQzEVt1SVtb6Ee');
+define('SECURE_AUTH_KEY',  'Nepot8M7IbV3rI8LjyEjddvFBIHxcLYUNiTneECnWuvfwzPxmLFEsndFm3svPPUr');
+define('LOGGED_IN_KEY',    '0HygKoE92CPBnLXyXzDqBzLmMqv9D8SUxgBPRrzvfXoF0mIvoVBXt9q1Ff6qPfWW');
+define('NONCE_KEY',        'ozyi6te8QgLFr1MHUHST1BBJFIDYuovu5ihxeBiCq7KJ4LHgh11tHrD2ERTWrfoE');
+define('AUTH_SALT',        'dF2fRf84CpEDJEJ6fitTTc3nW6JeInwEf2UWst8zxS9VPvWSvgHFNthKtrP5w77n');
+define('SECURE_AUTH_SALT', 'JStvgTJmKtFarEgxe8oLJWptsMFM6BQFApV1DIdvPsi8b9MYmwPx3fBJanoBfQF0');
+define('LOGGED_IN_SALT',   'VqoWT1MvzCyqI52ch4pDJrCLum8Iso8up5h5dM6dLT33bIwNWjG7C4RIUumNFe3M');
+define('NONCE_SALT',       'x2wmjzDYIuPNpCHh6YLJP05bxmDcJcazIvmzprQ4S92TzHMA7GHJB22eaAX7b5vy');
